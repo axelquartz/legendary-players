a29729ac15e53c359548cd9cdb135e7ad2fd4e4e
Axel <axelarep@gmail.com>
2022-08-30 22:23:12 -0500

    Add 4 player cards of Necaxa team from Mexico, with official information and alert Javascript functionality in each image of each card The structure of the cards is complete, but they dont have any deep CSS styling yet.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # legendary-players
+
+This is a super small project, which is helping me to remember Javascript fundamentals.
--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-let screen = window.document;
+let alex = document.getElementById('alex');
+let ivo = document.getElementById('ivo')
+let agustin = document.getElementById('agustin')
+let pelaez = document.getElementById('pelaez')
 
-screen.addEventListener('click', function () {
-    alert('Haaland')    
-})
 
+function alexClick(){
+    alert('Alex Ferguson: "Aguinaga es mejor que Kross"')
+}
+
+function ivoClick() {
+    alert('Johann Cruyff: "Ivo cabeceaba mejor que Cristiano Ronaldo"')
+}
+
+function agustinClick() {
+    alert('Lionel Messi: "preferiría tener al Tín Delgado de compañero que a MBappé"')
+}
+
+function pelaezClick() {
+    alert('Josep Guardiola: "Ricardo es como Ibrahimovic pero más atlético"')
+}
+
+alex.addEventListener('click', alexClick)
+ivo.addEventListener('click', ivoClick)
+agustin.addEventListener('click', agustinClick)
+pelaez.addEventListener('click', pelaezClick)
